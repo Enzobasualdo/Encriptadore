7f59a0e55b5dedf1b6d53d9e3974c3d75d07f2b0
Enzobasualdo <165123357+Enzobasualdo@users.noreply.github.com>
2024-05-19 12:03:37 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -44,7 +44,7 @@ function desencriptar() {
         policia.src = "rocagif.gif";
     }
     else {
-        policia.src = "policia.png";
+        policia.src = "travolta.png";
         alert("Debes ingresar algun texto")
         tituloMensaje.textContent = "ningun mensaje encriptado"
         parrafo.textContent = "Ingrese el mensaje a encriptar";
